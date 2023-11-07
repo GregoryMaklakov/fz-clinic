476904a21eb8a015000ae273ceecbd76871e4caa
Gregory <maklakov.grigory@gmail.com>
2023-11-07 17:42:40 +0100

    add adaptive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,14 +26,14 @@ export function Footer() {
         setHoveringLink(false);
     };
     return (
-        <footer className="w-full max-w-[1920px] z-[100] block mx-auto bg-light dark:bg-dark py-8 px-32 xl:p-24 lg:px-16">
+        <footer className="w-full max-w-[1920px] z-[100] block mx-auto bg-light dark:bg-dark py-12 px-32 lg:px-16 md:px-6">
             <div className="min-w-full mx-auto inline-block dark:bg-light bg-dark p-8 text-light/75 dark:text-dark rounded-3xl">
-                <div className="footer-top grid w-full grid-cols-2">
+                <div className="footer-top grid w-full grid-cols-2 xl:flex xl:flex-col-reverse">
                     <div className="fill-black dark:fill-dark pb-8 col-span-1">
-                        <Icon name="fz-clinic" className="w-[400px] h-auto" />
+                        <Icon name="fz-clinic" className="w-[400px] h-auto sm:w-full" />
                     </div>
 
-                    <div className="w-full col-span-1 grid grid-cols-2">
+                    <div className="w-full col-span-1 grid grid-cols-2 sm:grid-cols-1">
                         <div className="col-span-1 flex flex-col">
                             {navLinks.map(link => (
                                 <CustomLink
@@ -45,8 +45,8 @@ export function Footer() {
                             ))}
                         </div>
 
-                        <div className="col-span-1 flex flex-col justify-between pb-8">
-                            <div className="flex items-end justify-between max-w-[290px]">
+                        <div className="col-span-1 flex flex-col justify-between pb-8 xl:gap-8">
+                            <div className="flex items-end justify-between max-w-[290px] sm:gap-0">
                                 <div>
                                     <h3 className="font-bold mb-2">Години роботи</h3>
                                     <ul className="">
@@ -62,22 +62,24 @@ export function Footer() {
                                 </ul>
                             </div>
 
-                            <div className="flex items-center">
-                                {socialLinks.map(link => (
-                                    <motion.a
-                                        key={link.name}
-                                        className="mr-3 dark:fill-dark/75 fill-light/75"
-                                        href={link.url}
-                                        target="_blank"
-                                        whileHover={{ y: -2 }}
-                                        whileTap={{ scale: 0.9 }}
-                                        onMouseEnter={handleMouseEnter}
-                                        onMouseLeave={handleMouseLeave}
-                                        aria-label={link.ariaLabel}
-                                    >
-                                        <Icon name={link.name} />
-                                    </motion.a>
-                                ))}
+                            <div className="flex items-center 2xl:flex-col 2xl:gap-3 2xl:self-start">
+                                <div className="flex items-center 2xl:self-start ">
+                                    {socialLinks.map(link => (
+                                        <motion.a
+                                            key={link.name}
+                                            className="mr-3 dark:fill-dark/75 fill-light/75  md:w-11 md:h-11 "
+                                            href={link.url}
+                                            target="_blank"
+                                            whileHover={{ y: -2 }}
+                                            whileTap={{ scale: 0.9 }}
+                                            onMouseEnter={handleMouseEnter}
+                                            onMouseLeave={handleMouseLeave}
+                                            aria-label={link.ariaLabel}
+                                        >
+                                            <Icon name={link.name} size={44} />
+                                        </motion.a>
+                                    ))}
+                                </div>
                                 <Link href="tel:380987775580" className="">
                                     <span className="hover:underline"> tel: +38 098 777 55 80</span>
                                 </Link>
@@ -85,14 +87,14 @@ export function Footer() {
                         </div>
                     </div>
                 </div>
-                <div className="footer-bottom flex items-center justify-between pt-8 border-t-[1px] dark:border-t-dark/25 border-t-light/25">
+                <div className="footer-bottom flex items-center justify-between pt-8 border-t-[1px] dark:border-t-dark/25 border-t-light/25 lg:flex-col lg:gap-3">
                     <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
                     <Link
                         href={socialLink.googleReview}
                         onMouseEnter={handleMouseEnter}
                         onMouseLeave={handleMouseLeave}
                         target="_blank"
-                        className="flex items-center xs:text-sm"
+                        className="flex items-center "
                     >
                         Залиш відгук{" "}
                         <span className="text-primary text-2xl px-1 dark:text-primaryDark">
@@ -107,7 +109,7 @@ export function Footer() {
                         onMouseLeave={handleMouseLeave}
                         target="_blank"
                     >
-                        <span className="underline underline-offset-2 xs:text-sm">
+                        <span className="underline underline-offset-2">
                             Першотравнева 51
                         </span>{" "}
                         | Кременчук
