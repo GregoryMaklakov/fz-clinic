18a7a5d03bb5366dceead9fd04e44edd2d585f21
Gregory <maklakov.grigory@gmail.com>
2024-02-29 16:47:03 +0100

    chnge meta url

--- src/pages/articles/[slug].jsx
+++ src/pages/articles/[slug].jsx
@@ -39,7 +39,7 @@ function ArticlePage() {
         />
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content="Про нас | FZ Clinic" />
-        <meta property="og:url" content="https://ginger-beauty-zone.com" />
+        <meta property="og:url" content="https://www.fz-clinic.com/" />
         <meta
           property="og:image"
           content="https://d375139ucebi94.cloudfront.net/region2/pl/162702/logo/87abe39eef5d4f5cb4968854db35be-ginger-beauty-zone-logo-0b4580ef172240329bb5c6e7f314b3-booksy.jpeg"
